4a32278299ede1733d298927fd97a3f5c002e8fd
Evgenichka <138754983+Evgenichka@users.noreply.github.com>
2024-01-30 23:41:51 +0300

    Update Toys.java
    Создали класс и конструктор

--- Toys.java
+++ Toys.java
@@ -1,1 +1,11 @@
+public class Toys {
+   public int id;
+   public String name;
+   public int qty;
+
+   public Toy(int id, String name, int qty) {
+        this.id = id;
+        this.name = name;
+        this.qty = qty;
+    }
 
